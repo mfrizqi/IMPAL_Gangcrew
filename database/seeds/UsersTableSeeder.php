d83653154e2729dd80a97028367862daa7637cb8
Muhammad Diaz <muhamaddiaz10@gmail.com>
2018-11-24 22:49:42 +0700

    User Seeder basic

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = factory('App\User')->make();
+        User::insert([
+            'name' => $user->name,
+            'email' => $user->email,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10)
+        ]);
+    }
+}
