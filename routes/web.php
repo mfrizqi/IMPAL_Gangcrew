5e2c17886296f633ae6f7d4d51f7388303d36f1a
Muhammad Fajar Rizqi <muhammadfajarrizqi@gmail.com>
2018-11-26 23:02:41 +0700

    Uploading assets (images & icon), create Homee as Home page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/homee', function () {
+    return view('homee');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
